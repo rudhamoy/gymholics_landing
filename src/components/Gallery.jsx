48a27bc174fc6826a8c34168bf577917d5ed7c78
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-10-06 02:01:24 +0530

    resize photo

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,17 +5,17 @@ const Gallery = () => {
         <div>
             <h1 className="my-2 font-semibold text-center text-xl">Gallery</h1>
             <div className="grid grid-cols-3 gap-2">
-                <img src="gym1.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="showcase1.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="showcase2.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="showcase3.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="showcase4.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="gym3.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="gym4.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="gym5.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="gym6.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="gym7.JPG" alt="" className="w-[220px] sm:w-[300px]" />
-                <img src="gym8.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym1_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="showcase1_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="showcase2_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="showcase3_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="showcase4_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym3_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym4_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym5_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym6_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym7_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
+                <img src="gym8_50.JPG" alt="" className="w-[220px] sm:w-[300px]" />
             </div>
         </div>
     )
