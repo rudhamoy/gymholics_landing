c6d59c5d7a9b46ceafc66aca6ce1a6d9e30e0ec0
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-10-06 20:24:16 +0530

    uuuu

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
                 <div className="mt-36">
                     <h1 className="text-white sm:text-9xl text-6xl font-bold text-center heading">JOIN <br />GYMHOLICS</h1>
                     <button className="p-2 px-5 bg-cyan-500 text-white rounded-md cursor-pointer mt-4 mb-2">Join us today</button>
-                    <p className="text-xs text-gray-600 font-semibold">Transform in 3 months, <br/>offers valid till 10th Oct, <br/> for first 20 new members only! <span className="text-orange-600">13 left</span></p>
+                    <p className="text-xs text-gray-600 font-semibold">Transform in 3 months, <br/>offers valid till 10th Oct, <br/> for first 20 new members only! <span className="text-orange-600">11 left</span></p>
                 </div>
                 <img src="tinywow_gymholics3_50.png" alt="" className="h-[460px] sm:h-[600px] absolute left-24 sm:left-[50%] dropShadow" />
                 <div className="rounded-md bg-white p-3 sm:p-4 shadow-md absolute left-6 sm:left-60 top-16 heading ">
