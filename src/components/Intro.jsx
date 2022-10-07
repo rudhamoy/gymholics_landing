28321a11fccb5b85d286a40b6aba55ca83222f15
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-10-07 22:49:14 +0530

    customising text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="heading p-3">
-      <h1 className="text-2xl font-semibold uppercase text-gray-600">Do you know ?</h1>
-      <p className="text-base font-semibold mb-5 uppercase text-gray-600">You can actually reach your GOAL in <br/><span className="text-orange-600 text-2xl">70 days</span> or less!</p>
-      <p className="p-2 border rounded-md shadow-md"><span className="font-semibold text-xl">Gymholics</span> is a personal trainer & instructor focusing on customized and flexible gym programs for your goals and needs, whether that's weight loss, muscle building or just general fitness.</p>
+      <h1 className="text-2xl font-semibold uppercase text-gray-600 text-center">Do you know ?</h1>
+      <p className="text-base font-semibold my-5 uppercase text-gray-600">You can actually achieve your GOAL in <br/><span className="text-orange-600 text-2xl">70 days</span> or less!</p>
+
+      <p className="font-semibold p-2 my-10 border rounded-md shadow-md">Fill up the form below and get up to <span className="text-2xl text-cyan-600">40%</span> discounts/offers.</p>
+
+      <p className="p-2 mt-6"><span className="font-semibold text-xl">Gymholics</span> is a personalized trainer & instructor focusing on customized and flexible gym programs for your goals and needs, whether that's weight loss, muscle building or just general fitness.</p>
       <br />
       <h1 className="text-xl font-semibold my-6 uppercase text-gray-600">Why Gymholcis is best for you ?</h1>
       <ul className='list-disc px-4 heading'>
@@ -18,7 +21,6 @@ const Intro = () => {
       <br />
       <p className=" ">How would you like to become more motivated?</p>
       <br />
-      <p className="font-semibold p-2 border rounded-md shadow-md">Fill up the form below and get up to <span className="text-2xl text-orange-600">40%</span> discounts/offers.</p>
     </div>
   )
 }
