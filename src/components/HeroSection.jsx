88ee878ac8c01bc5398c2e39f483483328c7f3dc
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-10-08 13:18:47 +0530

    optimised landing page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,14 +5,16 @@ const HeroSection = () => {
         <div className="h-[60vh] sm:h-[90vh] overflow-x-hidden sm:overflow-x-visible">
             <div className="flex justify-center items-center h-[90%] sm:h-[95%] w-[100vw] bg-[#C2CFD6] relative">
                 
-                <div className="mt-36">
+                <div className="mt-36 z-40">
                     <h1 className="text-white sm:text-9xl text-6xl font-bold text-center heading">JOIN <br />GYMHOLICS</h1>
                     <button className="p-2 px-5 bg-cyan-500 text-white rounded-md cursor-pointer mt-4 mb-2">Join us today</button>
-                    <p className="text-xs text-gray-600 font-semibold">Transform in 3 months, <br/>offers valid till 10th Oct, <br/> for first 20 new members only! <span className="text-orange-600">7 left</span></p>
+                    <p className="text-xs text-gray-600 font-semibold">offers valid till 10th Oct, <br/> for first 20 new members only! <span className="text-orange-600">7 left</span></p>
                 </div>
-                <img src="tinywow_gymholics3_50.png" alt="" className="h-[460px] sm:h-[600px] absolute left-24 sm:left-[50%] dropShadow" />
+
+                <img src="tinywow_gymholics3_50.png" alt="" className="h-[430px] sm:h-[600px] absolute left-24 sm:left-[50%] z-10 dropShadow" />
+
                 <div className="rounded-md bg-white p-3 sm:p-4 shadow-md absolute left-6 sm:left-60 top-16 heading ">
-                    <p className="font-semibold">Get Offers <br />upto <span className="text-3xl font-bold text-yellow-500">40%</span></p>
+                    <p className="font-semibold text-sm">Get Offers <br />upto <span className="text-3xl font-bold text-yellow-500">40%</span></p>
                 </div>
             </div>
         </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,22 +3,25 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="heading p-3">
-      <h1 className="text-2xl font-semibold uppercase text-gray-600 text-center">Do you know ?</h1>
+      <h1 className="text-2xl font-semibold uppercase text-gray-600">Do you know ?</h1>
       <p className="text-base font-semibold mt-5 uppercase text-gray-600">You can actually achieve your GOAL in <br/><span className="text-orange-600 text-2xl">70 days</span> or less!</p>
-      <p className="text-sm my-2">Not sure ? Visit us to know how are we doing it</p>
+      <p className="text-sm my-2">Yes! that's possible for YOU with us in GYMHOLICS</p>
 
-    <p className='p-2 bg-yellow-500 shadow-md text-sm mt-12'>Thank You for Visiting this Page but make sure you don't miss this offers</p>
-      <p className="font-semibold p-2 mb-10 border rounded-md shadow-md">Fill up the form below and get up to <span className="text-2xl text-cyan-600">40%</span> discounts/offers.</p>
+    <p className='p-2 bg-yellow-500 shadow-md text-sm mt-12'>Thank You for Visiting this Page BUT make sure you don't miss this OFFERS</p>
+      <p className="font-semibold p-2 mb-10 border rounded-md shadow-md">Fill up the form below and get up to <span className="text-2xl text-yellow-500">40%</span> discounts/offers.</p>
 
       <h1 className="text-xl font-semibold my-2 uppercase mt-6 text-gray-600">Who are we ?</h1>
       <p className=""><span className="font-semibold text-xl">Gymholics</span> is a personalized trainer & instructor focusing on customized and flexible gym programs for your goals and needs, whether that's weight loss, muscle building or just general fitness.</p>
       <br />
       <h1 className="text-xl font-semibold my-6 uppercase text-gray-600">Why Gymholcis is best for you ?</h1>
       <ul className='list-disc px-4 heading'>
-        <li className="my-3 leading-7"> We offer several types of packages to suit all budgets, for both women and men who seek for a better and healthier lifestyle.
+        <li className="my-3 leading-7 text-sm"><span className="font-semibold text-base">Living healthier lifestyle is your dream?</span>
+        <br/> We offer several types of packages to suit all budgets, for both women and men who seek for a better and healthier lifestyle.
         </li>
-        <li className="my-3 leading-7">Our goal is to help you achieve your fitness goals easily by customizing your training plan with the best suitable gym regimen</li>
-        <li className="my-3 leading-7">We ensure that each of our clients get appropriate guidance and motivation all throughout their workouts, which are conducted at our high quality fitness center equipped with the top of the line machines, together with group fitness classes to give you a fun experience while getting fit.</li>
+        <li className="my-3 leading-7 text-sm"><span className="font-semibold text-base">How easy to achieve your fitness goal?</span>
+        <br/>Our goal is to help you achieve your fitness goals easily by customizing your training plan with the best suitable gym regimen</li>
+        <li className="my-3 leading-7 text-sm"><span className="font-semibold text-base">Guidance and Motivation check?</span>
+        <br/>We ensure that each of our clients get appropriate guidance and motivation all throughout their workouts, which are conducted at our high quality fitness center equipped with the top of the line machines, together with group fitness classes to give you a fun experience while getting fit.</li>
       </ul>
       <br />
       <br />
